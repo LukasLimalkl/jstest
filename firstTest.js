411289e543fb795de8765986c91b8fee9fc14551
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-07 21:30:46 -0300

    feat: first test

---
+++ firstTest.js
@@ -0,0 +1,19 @@
+// Questao 1 do teste
+
+function SOMA() {
+
+const INDICE = 13
+
+let soma = 0
+
+let k = 0
+
+    while(k < INDICE){
+        k++
+        soma = soma + k
+    }
+    console.log(soma)
+
+}
+
+SOMA()
